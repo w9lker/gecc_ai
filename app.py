f1ce7a2a82ffb4fafd09f70fdde85952a2e09e83
w9lker <skhalikov2@gmail.com>
2025-10-01 10:09:05 +0800

    fix

--- app.py
+++ app.py
@@ -316,8 +316,7 @@ def render_page_1():
                 When prompting Lyria 2 it's helpful to consider the overall style of music you want to generate. Consider options such as: classical, electronic, rock, jazz, hip hop, or pop. You can even describe more general styles that include cinematic, ambient, or lo-fi.
                 With Lyria 2, you can generate a 30 second WAV audio at a 48kHz sample rate from a text prompt. In order to generate an audio clip in the following sample, specify the following info:
                 
-                With this in mind, please give me back a pure finetuned prompt, given {prompt}.
-                Be as detailed and avoid short or long prompts.
+                With this in mind, please give me back a pure finetuned prompt, given the original prompt: {prompt}.
             """
         try:
             client = genai.Client(api_key=st.secrets["gemini"]["api_key"])
