a4cfac5d8a075db6af940a015e73907d7020a4bb
w9lker <skhalikov2@gmail.com>
2025-10-08 21:12:10 +0800

    fix

--- app.py
+++ app.py
@@ -316,7 +316,7 @@ def render_page_1():
                 When prompting Lyria 2 it's helpful to consider the overall style of music you want to generate. Consider options such as: classical, electronic, rock, jazz, hip hop, or pop. You can even describe more general styles that include cinematic, ambient, or lo-fi.
                 With this in mind, please give me back a detailed pure finetuned prompt, given the original prompt: {prompt}.
                 Again just return the finetuned prompt (no extra punctuation or markdown), make the prompt around 100 words and describe the music through style, instruments, genre, tone, intensity.
-                Be aware that the prompt should include only the standard english words, since the model doesn't support foreign words.
+                Be aware that the prompt should include only the standard english words, since the model doesn't support foreign words. Describe the style through the standard english words.
             """
         try:
             client = genai.Client(api_key=st.secrets["gemini"]["api_key"])
